b5fd24ccc859a08ef1d5ccceb5b34c958e22beec
bender1911 <tudum@bk.ru>
2020-05-07 11:26:02 +0300

    add models and activate app(add app to settings.py)

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='notes_categories')
+    
+    class Meta:
+        ordering = ('name',)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, blank=False)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    rubric = models.ForeignKey(Category, on_delete=models.CASCADE,
+                               related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='notes_posts')
+    salt = models.BinaryField(blank=True)
+    is_secret = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('-created',)
