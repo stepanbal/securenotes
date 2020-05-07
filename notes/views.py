db89919068ecfcd6cd92b2ab9ec73b008a44fb7e
bender1911 <tudum@bk.ru>
2020-05-07 16:04:15 +0300

    add view and template for category list. It is the index page

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Category
+
 
 def index(request):
-    pass
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'notes/index.html', context)
