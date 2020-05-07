2e065790b11c955ede77d1b1b58d10b7cad5c30f
bender1911 <tudum@bk.ru>
2020-05-07 17:38:34 +0300

    add view and template for adding of new category

--- notes/urls.py
+++ notes/urls.py
@@ -7,4 +7,5 @@ app_name = 'notes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:cat_id>', views.category_detail, name='category'),
+    path('category/add/', views.category_add, name='category_add'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Category
+from .forms import AddCategoryForm
 
 
 def index(request):
@@ -12,3 +14,16 @@ def category_detail(request, cat_id):
     category = get_object_or_404(Category, pk=cat_id)
     context = {'category': category}
     return render(request, 'notes/category.html', context)
+
+
+def category_add(request):
+    if request.method == 'POST':
+        add_form = AddCategoryForm(data=request.POST)
+        if add_form.is_valid():
+            new_category = add_form.save(commit=False)
+            new_category.author = User.objects.get(pk=1)
+            new_category.save()
+            return render(request, 'notes/add_category.html', {'added': True})
+    else:
+        add_form = AddCategoryForm()
+        return render(request, 'notes/add_category.html', {'add_form': add_form})
