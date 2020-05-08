85fc945bb8a09b9d2f91a1551eb72c0899f57c93
bender1911 <tudum@bk.ru>
2020-05-08 12:12:07 +0300

    fix templates (buttons now outline)

--- notes/urls.py
+++ notes/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:cat_id>/', views.category_detail, name='category'),
     path('category/add/', views.category_add, name='category_add'),
+    path('category/del/<int:cat_id>/', views.category_delete, name='category_delete'),
     path('post/<int:post_id>/', views.post_detail, name='post'),
     path('post/add/', views.post_add, name='post_add'),
     path('post/del/<int:post_id>/', views.post_delete, name='post_delete'),
-    path('category/del/<int:cat_id>/', views.category_delete, name='category_delete'),
 ]
