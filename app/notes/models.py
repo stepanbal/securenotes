ca301de68e68051cc9fa436976d1640b88dd3699
stepanbal <stepanb@tut.by>
2020-05-18 12:07:27 +0300

    add type hinting into models.py

--- app/notes/models.py
+++ app/notes/models.py
@@ -1,33 +1,35 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=250, blank=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='notes_categories')
-    
-    def __str__(self):
+    name: str = models.CharField(max_length=250, blank=False)
+    author: str = models.ForeignKey(User, on_delete=models.CASCADE,
+                                    related_name='notes_categories')
+
+    def __str__(self: 'Category') -> str:
         return self.name
-    
+
     class Meta:
-        ordering = ('name',)
+        ordering: tuple = ('name',)
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=250, blank=False)
-    body = models.TextField(blank=True)
-    secure_body = models.BinaryField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    rubric = models.ForeignKey(Category, on_delete=models.CASCADE,
-                               related_name='posts')
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name='notes_posts')
-    salt = models.BinaryField(blank=True)
-    is_secret = models.BooleanField(default=True)
+    title: str = models.CharField(max_length=250, blank=False)
+    body: str = models.TextField(blank=True)
+    secure_body: bytes = models.BinaryField(blank=True)
+    created: datetime = models.DateTimeField(auto_now_add=True)
+    updated: datetime = models.DateTimeField(auto_now=True)
+    rubric: Category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                         related_name='posts')
+    author: User = models.ForeignKey(User, on_delete=models.CASCADE,
+                                     related_name='notes_posts')
+    salt: bytes = models.BinaryField(blank=True)
+    is_secret: bool = models.BooleanField(default=True)
 
-    def __str__(self):
+    def __str__(self: 'Post') -> str:
         return self.title
 
     class Meta:
