dd0c07bff622d73473b29145ee1181a9a5a99926
stepanbal <stepanb@tut.by>
2020-05-19 10:44:02 +0300

    add tests for models

--- app/notes/tests.py
+++ app/notes/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Category, Post
+
+
+class CategoryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='user')
+        Category.objects.create(name='Work', author=user)
+
+    def test_category_name_is_field_name(self):
+        cat = Category.objects.get(id=1)
+        expected_object_name = cat.name
+        self.assertEquals(expected_object_name, str(cat))
+
+
+class PostModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='user')
+        cat = Category.objects.create(name='Work', author=user)
+        Post.objects.create(title='Work 1', author=user, rubric=cat)
+
+    def test_post_name_is_field_title(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = post.title
+        self.assertEquals(expected_object_name, str(post))
